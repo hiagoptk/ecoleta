5feb424fdac94ac486f0f3cf10989de500d86461
hiagoptk <hiogaptk@gmail.com>
2020-06-06 15:08:25 -0300

    Criando toda a parte mobile com Expo e ReactNative, definindo a home, e as rotas da app. Pre-definindo também as telas do app

---
+++ mobile/src/pages/Detail/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {View, Text, Image, StyleSheet, ImageBackground, } from "react-native";
+
+const Detail = () => {
+  return <View />
+
+};
+
+export default Detail;
---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {View, Text, Image, StyleSheet, ImageBackground, } from "react-native";
+
+const Points = () => {
+  return <View />
+
+};
+
+export default Points;
